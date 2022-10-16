282f7b77625cd620d087d709fab7fc2db42334c5
Ing. Di Filippo <davidedifilippo@gmail.com>
2022-10-16 18:18:54 +0200

    Create main.cpp
    First working code

---
+++ main.cpp
@@ -0,0 +1,185 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial serial1(7,10);
+ 
+// Uncomment whatever type you're using!
+static char recv_buf[512];
+static bool is_exist = false;
+static bool is_join = false;
+static int led = 0;
+
+static void recv_phrase(char *p_msg)
+{
+    if (p_msg == NULL)
+    {
+        return;
+    }
+    char *p_start = NULL;
+    int data = 0;
+    int rssi = 0;
+    int snr = 0;
+ 
+    p_start = strstr(p_msg, "RX");
+    if (p_start && (1 == sscanf(p_start, "RX: \"%d\"\r\n", &data)))
+    {
+        Serial.println(data);
+        
+        
+        led = !!data;
+    
+        if (led)
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+        }
+        else
+        {
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+    }
+ 
+    p_start = strstr(p_msg, "RSSI");
+    if (p_start && (1 == sscanf(p_start, "RSSI %d,", &rssi)))
+    {
+        Serial.println(rssi);
+    
+    }
+    p_start = strstr(p_msg, "SNR");
+    if (p_start && (1 == sscanf(p_start, "SNR %d", &snr)))
+    {
+    Serial.println(rssi);
+    }
+}
+
+static int at_send_check_response(const char *p_ack, int timeout_ms, const char *p_cmd, ...)
+{
+    int ch;
+    int num = 0;
+    int index = 0;
+    int startMillis = 0;
+    va_list args;
+
+    memset(recv_buf, 0, sizeof(recv_buf)); //Azzera il buffer di ricezione
+
+    va_start(args, p_cmd);
+    serial1.printf(p_cmd, args); 
+    Serial.printf(p_cmd, args); 
+    va_end(args);
+
+    delay(200);
+    startMillis = millis();
+ 
+    if (p_ack == NULL)
+    {
+        return 0;
+    }
+ 
+    do
+    {
+        while (serial1.available() > 0)
+        {
+            ch = serial1.read();
+            recv_buf[index++] = ch;
+            Serial.print((char)ch);
+            delay(2);
+        }
+ 
+        if (strstr(recv_buf, p_ack) != NULL)
+        {
+            return 1;
+        }
+ 
+    } while (millis() - startMillis < timeout_ms);
+    return 0;
+}
+ 
+
+
+void setup(){
+
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+ 
+    serial1.begin(9600);
+    Serial.print("E5 LORAWAN TEST\r\n");
+
+     if (at_send_check_response("+AT: OK", 100, "AT\r\n"))
+    {
+        is_exist = true;
+        at_send_check_response("+ID: AppEui", 1000, "AT+ID\r\n");
+        at_send_check_response("+MODE: LWOTAA", 1000, "AT+MODE=LWOTAA\r\n");
+        at_send_check_response("+DR: EU868", 1000, "AT+DR=EU868\r\n");
+        at_send_check_response("+CH: NUM", 1000, "AT+CH=NUM,0-2\r\n");
+        at_send_check_response("+KEY: APPKEY", 1000, "AT+KEY=APPKEY,\"21417788BF9AC55B0E1F704593CDC050\"\r\n");
+        at_send_check_response("+CLASS: C", 1000, "AT+CLASS=A\r\n");
+        at_send_check_response("+PORT: 8", 1000, "AT+PORT=8\r\n");
+        delay(200);
+       
+        is_join = true;
+    }
+    else
+    {
+        is_exist = false;
+        Serial.print("No E5 module found.\r\n");
+      
+    }
+}
+
+
+
+void loop(void)
+{
+    float temp = 0;
+    float humi = 0;
+ 
+    temp = random(0,100);
+    humi = 71;
+ 
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temp);
+    Serial.println(" *C");
+ 
+ 
+    if (is_exist)
+    {
+        int ret = 0;
+        if (is_join)
+        {
+ 
+            ret = at_send_check_response("+JOIN: Network joined", 12000, "AT+JOIN\r\n");
+            if (ret)
+            {
+                is_join = false;
+            }
+            else
+            {
+                at_send_check_response("+ID: AppEui", 1000, "AT+ID\r\n");
+                Serial.print("JOIN failed!\r\n\r\n");
+                delay(5000);
+            }
+        }
+        else
+        {
+            char cmd[128];
+            sprintf(cmd, "AT+MSG=\"%d\"\r\n", (int)temp);
+            ret = at_send_check_response("Done", 5000, cmd);
+            if (ret)
+            {
+                recv_phrase(recv_buf);
+            }
+            else
+            {
+                Serial.print("Send failed!\r\n\r\n");
+            }
+            delay(10000);
+        }
+    }
+    else
+    {
+        delay(1000);
+    }
+}
